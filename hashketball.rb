9cf4218f06914a29f0156c964d6cb15c8866aa3e
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-11-09 04:56:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,15 @@
 def game_hash
   {
-    
+    home: {
+      team_name: {},
+      colors: {},
+      players: {}
+    },
+    away: {
+      team_name: {},
+      colors: {},
+      players: {}
+    }
   }
 end
 
